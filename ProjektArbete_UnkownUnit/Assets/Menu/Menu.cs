d13e98ddca1fe05bcc1776d22f8b55fce2ea5d88
André Topalovic Törnström <tornstromandre@gmail.com>
2018-11-27 09:25:58 +0100

    Added a menu scene and an exit button

---
+++ ProjektArbete_UnkownUnit/Assets/Menu/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+			if (hit.collider != null)
+			{
+				switch(hit.collider.tag)
+				{
+					case "ExitButton":
+						Application.Quit();
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+}
