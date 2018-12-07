e5d210d2a3773e30c9952a0b33a760434a07eafe
Johan Svensson <johan.svensson93@outlook.com>
2018-12-07 11:33:18 +0100

    MapGenerator added

---
+++ ProjektArbete_UnkownUnit/Assets/Entities/NewEnvironment.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEnvironment : MonoBehaviour
+{
+    public int Wave;
+    public List<Tower> Towers;
+    public List<Enemy> Enemies;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
--- ProjektArbete_UnkownUnit/Assets/Scripts/Map/MapManager.cs
+++ ProjektArbete_UnkownUnit/Assets/Scripts/Map/MapManager.cs
@@ -1,24 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour {
+public class MapManager : MonoBehaviour
+{
 
     [SerializeField]
-    private GameObject tile;
+    private GameObject[] tilePrefabs = new GameObject[] { };
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float TileSize { get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; } }
+
+    // Use this for initialization
+    void Start()
+    {
+        CreateMap();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     private void CreateMap()
     {
+        string[] mapData = ReadMapText();
 
+        int mapX = mapData[0].ToCharArray().Length;
+        int mapY = mapData.Length;
+
+        Vector3 mapStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+
+        for (int y = 0; y < mapY; y++)
+        {
+            char[] newTiles = mapData[y].ToCharArray();
+
+            for (int x = 0; x < mapX; x++)
+            {
+                PlaceTile(newTiles[x].ToString(), x, y, mapStart);
+            }
+        }
+    }
+
+    private void PlaceTile(string tileArray, int x, int y, Vector3 mapStart)
+    {
+        int tileIndex = int.Parse(tileArray);
+
+        GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
+
+        //Changes the position of newTile
+        newTile.transform.position = new Vector3(mapStart.x + (TileSize * x), mapStart.y - (TileSize * y), 0);
+    }
+
+    //Reads from TextFile
+    private string[] ReadMapText()
+    {
+        TextAsset data = Resources.Load("Map") as TextAsset;
+        string newData = data.text.Replace(Environment.NewLine, string.Empty);
+
+        return newData.Split('-');
     }
 }
