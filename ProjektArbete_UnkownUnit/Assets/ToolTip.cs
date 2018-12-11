e0929f4857f4e33368ba715d9d9e147c72d5c49e
Christoffer Ljungqvist <christoffer.ljungqvist@hotmail.se>
2018-12-11 12:17:53 +0100

    Added  tooltip

---
+++ ProjektArbete_UnkownUnit/Assets/ToolTip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Entities;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour
+{
+    [SerializeField]
+    private Text toolTiptText;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    toolTiptText.text = "Name: " + Tower.Instance.Name+"\n"+
+	        "Damage: " + Tower.Instance.Damage + "\n"+
+	        "Description: " + Tower.Instance.Description;
+	}
+}
