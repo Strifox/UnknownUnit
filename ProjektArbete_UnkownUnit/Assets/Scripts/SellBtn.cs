745512554db521a1f6cfc9c29538aa35bc24a6da
André Topalovic Törnström <tornstromandre@gmail.com>
2018-12-11 14:51:58 +0100

    Fixed tooltip visibility and other

--- ProjektArbete_UnkownUnit/Assets/Scripts/SellBtn.cs
+++ ProjektArbete_UnkownUnit/Assets/Scripts/SellBtn.cs
@@ -37,7 +37,7 @@ public class SellBtn : MonoBehaviour
 			Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
 			RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-			if (hit.collider != null)
+			if (hit.collider != null && hit.collider.tag == "Tower")
 			{
 				SelectedTower = hit.collider.gameObject.GetComponent<Tower>();
 				Payback = this.SelectedTower.TotalCost / 4;
